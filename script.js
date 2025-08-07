defa287c60003d1e304bbbd4a1408353e0afec5c
OTIMEYIN <kessdreams@gmail.com>
2025-08-07 13:30:27 +0100

    added script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+let x = 5;
+let y = 4;
+let z = x + y;
+let myName = "otimeyin"
+let gameName =
+console.log(x);
+console.log(y);
+console.log(z);
+console.log(myName);
+console.log(gameName);
+let a = "5" + 5;
+console.log(a);
+console.log(typeof(a));
+
+function myfunction(a, b) {
+    return a + b;
+}
+console.log(myfunction(5, 5))
